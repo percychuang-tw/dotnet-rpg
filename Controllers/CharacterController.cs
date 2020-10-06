524e4a6860443491d04c4de8ec3e7600bbb65ad3
Percy_Chuang <percyeor@yahoo.com.tw>
2020-10-06 22:59:54 +0800

    remark commit.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,38 +1,36 @@
-using System.Collections.Generic;
-using System.Linq;
 using dotnet_rpg.Models;
 using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace dotnet_rpg.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
-    public class CharacterController:ControllerBase
+    public class CharacterController : ControllerBase
     {
         private readonly ICharacterService _characterService;
 
-        public CharacterController(ICharacterService characterService) 
+        public CharacterController(ICharacterService characterService)
         {
             _characterService = characterService;
         }
 
 
         [HttpGet("GetAll")]
-        public IActionResult Get() 
+        public IActionResult Get()
         {
             return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetSingle(int id) 
+        public IActionResult GetSingle(int id)
         {
             return Ok(_characterService.GetCharacterById(id));
-        }        
+        }
 
         [HttpPost]
-        public IActionResult AddCharacter(Character newCharacter) 
+        public IActionResult AddCharacter(Character newCharacter)
         {
             return Ok(_characterService.AddCharacter(newCharacter));
         }
