aa2ed43487b8c00fdda9fad1add4f03e3d3f4cb6
Percy_Chuang <percyeor@yahoo.com.tw>
2020-10-06 18:27:40 +0800

    2-16 Character Service.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,37 +1,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class characterController:ControllerBase
+    public class CharacterController:ControllerBase
     {
-        private static List<Character> characters = new List<Character> {
-            new Character(),
-            new Character {Id = 1, Name = "Sam"}
-        };
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService) 
+        {
+            _characterService = characterService;
+        }
+
 
         [HttpGet("GetAll")]
         public IActionResult Get() 
         {
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
         public IActionResult GetSingle(int id) 
         {
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+            return Ok(_characterService.GetCharacterById(id));
         }        
 
         [HttpPost]
-        public IActionResult AddCharacter(Character newCharacter ) 
+        public IActionResult AddCharacter(Character newCharacter) 
         {
-            characters.Add(newCharacter);
-            return Ok(characters);    
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
+
     }
 
 }
